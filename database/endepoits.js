a07cef37675a0b187b9fb8868163a6bd8a7fcbc2
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2022-08-21 17:19:58 -0300

    Arrumando algumas coisas

--- database/endepoits.js
+++ database/endepoits.js
@@ -1,28 +1,28 @@
 const conecatarBD = require('./script');
+require('./cachorros');
+require('./gatos');
+require('./pessoas');
 
 const getDog = async() => {
     const cliente = await conecatarBD();
-    const BD = cliente.BD('dataBase');
-    const colecao = BD.colecao('cachorros');
-    const dogs = colecao.find({}).limit(10).toArray();
+    const BD = cliente.BD(cachorros);
+    const dogs = BD.find({}).limit(10).toArray();
     await cliente.close();
     return dogs;
 }
 
 const getCats = async() => {
     const cliente = await conecatarBD();
-    const BD = cliente.BD('dataBase');
-    const colecao = BD.colecao('gatos');
-    const cats = await colecao.find({}).limit(10).toArray();
+    const BD = cliente.BD(gatos);
+    const cats = await BD.find({}).limit(10).toArray();
     await cliente.close();
     return cats;
 }
 
 const getUser = async() => {
     const cliente = await conecatarBD();
-    const BD = cliente.BD('dataBase');
-    const colecao = BD.colecao('pessoas');
-    const user = await colecao.find({}).limit(10).toArray();
+    const BD = cliente.BD(pessoas);
+    const user = await BD.find({}).limit(10).toArray();
     await cliente.close();
     return user;
 }

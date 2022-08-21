44b1a84470e1c0abbde5d63de51cc135a18b8c3d
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2022-08-21 17:15:04 -0300

    Arrumando Identacao

--- index.js
+++ index.js
@@ -7,28 +7,32 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/' , (req, res) => {
-   res.send(
-    `<h1>
-        Servidor Web em nuvem
-    </h1>
+   res.send(`<h1> Servidor Web em nuvem </h1>
     <h2>
        GCC180 -  Computacao em Nuvem -Ufla    
     </h2>
-    <h3>
         Gabriel Nathan Almeida Silva
-    </h3>
-
     <br>
+     <br>
+      <br>
         <h1>
             End Poinst disponiveis no momento
         </h1>
+        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/dogs'">
+            <h3>/dogs<h3>
+         </button>
+        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/cats'">
+            <h3>/cats<h3></button>
+        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/users'">
+            <h3>/users<h3>
+        </button>
+        `)
+    }
+  );
 
-        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/dogs'"><h3>/dogs<h3></button>
-        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/cats'"><h3>/cats<h3></button>
-        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/users'"><h3>/users<h3></button>
-    `
-   )
-    
+app.get('/gatos', async(req , res)=>{
+    const cats = await getCats();
+    res.send(cats);
 });
 
 app.get('/pessoas', async(req , res)=>{
@@ -41,9 +45,4 @@ app.get('/cachorros', async(req , res)=>{
     res.send(dogs);
 });
 
-app.get('/gatos', async(req , res)=>{
-    const cats = await getCats();
-    res.send(cats);
-});
-
-app.listen(PORT,() => console.log('listening on port ' + PORT));
+app.listen(PORT,() => console.log('Escutando na porta ' + PORT));

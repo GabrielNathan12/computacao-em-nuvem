caae27dbfba0a148fd384e58620747ca2e86440c
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2022-08-21 06:39:24 -0300

    Finalizando

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ const PORT = process.env.PORT || 3000;
 app.get('/' , (req, res) => {
    res.send(`<h1> Servidor Web em nuvem </h1>
     <h2>
-       GCC180 -  Computacao em Nuvem -Ufla    
+        GCC180 -  Computacao em Nuvem -Ufla   
+        Gabriel Nathan Almeida Silva 
     </h2>
-        Gabriel Nathan Almeida Silva
+       
     <br>
      <br>
       <br>
         <h1>
             End Poinst disponiveis no momento
         </h1>
+        
         <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/dogs'">
             <h3>/dogs<h3>
-         </button>
+        </button>
+        
         <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/cats'">
             <h3>/cats<h3></button>
-        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/users'">
-            <h3>/users<h3>
+        
+        <button onclick="window.location.href = 'https://app-nuvem-test.herokuapp.com/user'">
+            <h3>/user<h3>
         </button>
         `)
     }
